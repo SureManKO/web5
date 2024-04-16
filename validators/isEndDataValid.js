e3c3db4386dd5cec6c97b1150669939d63568953
SureManKO <mshurmanko@mail.ru>
2024-04-16 14:29:08 +0500

    pipupu

---
+++ validators/isEndDataValid.js
@@ -0,0 +1,41 @@
+const { isObjectHasProps, isArrayHasLength } = require("./utils/validators");
+
+const validateFooterData = (data) => {
+  // Проверяем объект upperContainerData на наличие полей и соответствие типу "объект"
+  const upperContainerData = data.upperContainerData;
+  isObjectHasProps(upperContainerData, ["header", "cta", "href"]);
+
+  // Проверяем объект leftLowerContainerData на наличие полей и соответствие типу "объект"
+  const leftLowerContainerData = data.leftLowerContainerData;
+  isObjectHasProps(leftLowerContainerData, ["img", "adress","reservedRights"]);
+  isObjectHasProps(leftLowerContainerData.img, ["src", "alt"]);
+  isObjectHasProps(leftLowerContainerData.adress, ["street", "city"]);
+
+  // Проверяем массив subHrefData на наличие полей и соответствие типу "массив"
+  const subHrefData = data.subHrefData;
+  isArrayHasLength(subHrefData);
+  subHrefData.forEach((item) => isObjectHasProps(item, ["title", "href"]));
+
+  // Проверяем массив subCompanyData на наличие полей и соответствие типу "массив"
+  const subCompanyData = data.subCompanyData;
+  isArrayHasLength(subCompanyData);
+  subCompanyData.forEach((item) => isObjectHasProps(item, ["title", "href"]));
+
+  // Проверяем массив subContactsData на наличие полей и соответствие типу "массив"
+  const subContactsData = data.subContactsData;
+  isArrayHasLength(subContactsData);
+  subContactsData.forEach((item) => isObjectHasProps(item, ["title", "href"]));
+
+  // Проверяем массив rightLowerContainerData на наличие полей и соответствие типу "массив"
+  const rightLowerContainerData = data.rightLowerContainerData;
+  isArrayHasLength(rightLowerContainerData);
+  rightLowerContainerData.forEach((item) =>
+    isObjectHasProps(item, ["header", "sub"])
+  );
+
+  // Проверяем объект licenseData на наличие полей и соответствие типу "объект"
+  const licenseData = data.licenseData;
+  isObjectHasProps(licenseData, ["title"]);
+};
+
+module.exports = validateFooterData;
